f60984b6eea97e47183e00805766a76bf4af9a0d
vishesh99 <vishesh.ranpariya70@gmail.com>
2024-05-29 10:58:43 +0530

    this for any file

--- text_chunking.py
+++
@@ -1,6 +0,0 @@
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-
-def get_text_chunks(text):
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=1000)
-    chunks = text_splitter.split_text(text)
-    return chunks
